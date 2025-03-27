7cbf80c887b59e94ba04ec0a4c32f4112b6912c3
yunni0315 <doyunyee2424@gmail.com>
2025-03-27 18:42:59 +0900

    setting

---
+++ likelion13/src/main/java/com/springboot/likelion13/Likelion13Application.java
@@ -0,0 +1,13 @@
+package com.springboot.likelion13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Likelion13Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Likelion13Application.class, args);
+    }
+
+}
---
+++ likelion13/src/test/java/com/springboot/likelion13/Likelion13ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.likelion13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Likelion13ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
